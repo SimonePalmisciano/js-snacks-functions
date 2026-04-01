4a81c0efa137501e8315cdb7094d74872606d5ea
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-01 19:04:31 +0200

    completato esercizio bonus con date() e utilizzato gethour

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,37 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+function salutoEducato (name) {
+
+    const d = new Date();
+    let hour = d.getHours();
+    let saluti = '';
+
+    if (hour >= 0 && hour <= 13) {
+        saluti = 'Buongiorno'; 
+
+    } else if (hour > 14 && hour < 17) {
+        saluti = 'Buon Pomeriggio';
+        
+    } else if (hour > 17) {
+        saluti = 'Buonasera';
+        
+    }
+
+    return (saluti + ' ' + name);
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let salutoInBaseOra = salutoEducato(userName);
+
+console.log(salutoInBaseOra);
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
