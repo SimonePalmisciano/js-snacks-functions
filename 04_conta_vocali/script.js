3e1995481099dba1c9fcea2ea793a9207186c786
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-01 17:29:11 +0200

    completato esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,53 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+// ci serve una funzione che legga quante vocali ci sono nella stringa
+// quindi bisogna prendere la stringa.length e analizzarla lettera per lettera
+// si può fare questo con un ciclo for per controllare ogni lettera
+// se è una vocale in contatore diventa + 1 se non lo è non restituisce nulla
+
+// function contaVocali(stringa) {
+
+//     const contatore = 0;
+
+//     let singolaLettera = '';
+
+//     for (let z = 0; z < stringa.length; z++) {
+
+//         singolaLettera = stringa[z];
+
+//     }
+
+//     if (singolaLettera === ('a', 'e', 'i', 'o', 'u')) {
+//         contatore += 1;
+
+//     }
+
+//     return contatore;
+// }
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+// let vocaliPresenti = contaVocali(word);
+
+// console.log(vocaliPresenti);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+////////////////////////////////////////////////////////////////////////////
+// ALTRO METODO
+////////////////////////////////////////////////////////////////////////////
+
+function haVocali (stringa) {
+    
+    const vocali = stringa.match(/[aeiou]/gi); 
+
+    return vocali ? vocali.length: 0;
+}
+
+let vocaliPresenti = haVocali(word);
+
+console.log(vocaliPresenti);
