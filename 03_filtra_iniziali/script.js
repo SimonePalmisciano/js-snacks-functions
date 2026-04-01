da96ef5a61233553114beff1d624ca1808402759
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-01 17:02:26 +0200

    completato esercizio 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,42 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+// prima di tutto bisogna prendere ogni elemento dell'array ed analizzarlo
+
+// quindi si usa un ciclo per prendere tutti gli elementi
+// poi si fa un if che quando troverà quella determinata lettera pushera il nome in un array
+
+function solitiNomi(nome) {
+    const solitiNomiConA = []; // dichiarata una variabile con array vuoto in modo da inserire le iniziali dei nomi
+
+    for (let z = 0; z < names.length; z++) { // questo ciclo for assegno ogni nome dell'array 'names' ad un elemento
+
+        const element = names[z]; // ogni elemento è names di z che è l'indice e va avanti di 1 alla volta quindi z=0 element = Anna
+        // poi z=1 element = Luca poi z=2 element = Marco ecc...
+
+        for (let z = 0; z < 1; z++) { // questo ciclo di lunghezza 1 prende l'indice della prima lettera di ogni parola
+
+            const iniziali = element[z]; // qui iniziali sarà z=0 iniziali = A poi si ripete ogni volta che il ciclo sopra compie 1 giro
+
+            if (iniziali === 'A') {
+
+                solitiNomiConA.push(element)
+            }
+
+        }
+
+    }
+
+    return solitiNomiConA;
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+let nomiConA = solitiNomi();
+
+console.log(nomiConA);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
